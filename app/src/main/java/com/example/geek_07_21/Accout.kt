d62cd7d97bbe0b69781eef9cbb6d63201b087c7f
asuno <asunoyozola@gmail.com>
2024-07-20 16:50:55 +0900

    もう無理や

---
+++ app/src/main/java/com/example/geek_07_21/Accout.kt
@@ -0,0 +1,27 @@
+package com.example.geek_07_21
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Accout : AppCompatActivity() {
+
+    private val fm = supportFragmentManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_accout)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        fm.beginTransaction().apply {
+            replace(R.id.account_container,Login() )
+            commit()
+        }
+    }
+}
--- app/src/main/java/com/example/geek_07_21/Login.kt
+++ app/src/main/java/com/example/geek_07_21/Login.kt
@@ -1,6 +1,5 @@
 package com.example.geek_07_21
 
-import MainFragment
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -12,28 +11,19 @@ import androidx.fragment.app.Fragment
 
 
 
-class Eiken_Mode : Fragment() {
+class Login : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_eiken_mode, container, false)
+        return inflater.inflate(R.layout.fragment_login, container, false)
     }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         val button = view.findViewById<Button>(R.id.login)
         button.setOnClickListener {
-            // FragmentManagerの取得
-            val pfm = parentFragmentManager
-
-            // トランザクションの生成・コミット
-            val ft = pfm.beginTransaction()
-            ft.apply {
-                replace(R.id.MainContainer, MainFragment())
-                commit()
-            }
         }
     }
 }
